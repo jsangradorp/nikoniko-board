32cb538513e69483bd5fc059c9e5ac43e1efa716
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-07-15 00:32:13 +0200

    Rename "public" folder to "static"

--- staticRoutes.js
+++ staticRoutes.js
@@ -1,22 +1,22 @@
 exports.register = function (plugin, options, next) {        
-    // Tell Hapi to handle the public folder as a file directory
+    // Tell Hapi to handle the static folder as a file directory
     plugin.route({ 
         method: 'GET',
         path: '/images/{param*}',
-        handler: {directory: {path: 'public/images/'}}
+        handler: {directory: {path: 'static/images/'}}
     });
 
     // We expect those files to be availiable from the root of our web app
     plugin.route({
         method: 'GET',
         path: '/robots.txt',
-        handler: {file: {path: 'public/robots.txt'}}
+        handler: {file: {path: 'static/robots.txt'}}
     });
 
     plugin.route({
         method: 'GET',
         path: '/favicon.ico',
-        handler: {file: {path: 'public/favicon.ico'}}
+        handler: {file: {path: 'static/favicon.ico'}}
     });
 
     next();
