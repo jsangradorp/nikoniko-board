6158acde159b333d46473b027678855c292f0952
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-07-22 00:16:09 +0200

    Half-functioning boardview

--- app/views/boardRowView.js
+++ app/views/boardRowView.js
@@ -3,11 +3,30 @@
 
 var View = require('ampersand-view');
 var templates = require('../templates');
-
+var ReportedFeelingView = require('./reportedFeelingView');
+var ReportedFeelingModel = require('../models/reportedFeeling')
+var moment = require('moment');
+var app = require('ampersand-app');
 
 module.exports = View.extend({
     template: templates.includes.boardRowView,
     bindings: {
         'model.label': '[data-hook=person-name]'
+    },
+    render: function(options) {
+        this.renderWithTemplate();
+        var model = new ReportedFeelingModel({
+            person_id: this.model.id,
+            board_id: 1,
+            date: moment().format('YYYY-MM-DD')
+        });
+        model.fetch();
+        for (var i = 0 ; i < 8; i++) {
+            var el = document.createElement('td');
+            this.el.appendChild(el);
+            this.renderSubview(new ReportedFeelingView({
+                model: model
+            }), el);
+        }
     }
 });
