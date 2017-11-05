98a418e1210ac4c45570c6a153d3e91582728bb2
Julio Sangrador-Paton <jsangradorp@gmail.com>
2017-11-05 01:53:59 +0100

    Add local mode

--- run.sh
+++ run.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env sh
-API_URL="https://0.0.0.0:8443" node --no-warnings server.js
+
+# LOCAL="y" ./run.sh
+
+trap "kill_children" 0
+kill_children() {
+        echo
+            [ -n "$proxypid"  ] && kill -9 $proxypid 2> /dev/null
+
+}
+
+if [ "$LOCAL" = "y" -o "$LOCAL" = "1" -o "$LOCAL" = "t" ] ; then
+    API_URL="https://127.0.0.1:8443"
+    SELF_PORT="9080"
+    local-ssl-proxy --source 9443 --target 9080 & proxypid=$!
+fi
+
+export API_URL
+export SELF_PORT
+node --no-warnings server.js
