71d98822ff610d28819a45a33168646f3f540cd6
Julio Sangrador-Paton <julio.sangrador@schibsted.com>
2017-11-12 01:18:39 +0100

    Use SHA256 certs for local development

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ kill_children() {
 if [ "$LOCAL" = "y" -o "$LOCAL" = "1" -o "$LOCAL" = "t" ] ; then
     API_URL="https://127.0.0.1:8443"
     SELF_PORT="9080"
-    local-ssl-proxy --source 9443 --target 9080 & proxypid=$!
+    local-ssl-proxy --source 9443 --target 9080 --cert localhost.crt --key localhost.key & proxypid=$!
 fi
 
 export API_URL
